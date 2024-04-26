805b966868a1e1b54358036bbaa25aff54e849b3
Jaewon <choijawon99@gmail.com>
2024-04-26 17:12:07 +0900

    [ADD] 기본 환경 셋팅

---
+++ src/test/java/com/est/neonaduri/NeonaduriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.est.neonaduri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeonaduriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
