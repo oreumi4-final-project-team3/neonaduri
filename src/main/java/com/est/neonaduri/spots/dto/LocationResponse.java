4edbed68b664a3aa24d92260876f4d55c653d633
임성현 <shlim0287@gmail.com>
2024-04-30 14:54:12 +0900

    [FEAT] SPOTS DTO 초안 추가 (리팩토링 필요)

---
+++ src/main/java/com/est/neonaduri/spots/dto/LocationResponse.java
@@ -0,0 +1,20 @@
+package com.est.neonaduri.spots.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class LocationResponse {
+
+
+    private String title;
+    private String address;
+    private String areaName;
+    private String contentName;
+    private String firstImage;
+    private double mapX;
+    private double mapY;
+}
