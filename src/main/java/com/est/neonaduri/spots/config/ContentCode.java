a81eac2629d9b3df9c69d94683366cc293d3f2a0
임성현 <shlim0287@gmail.com>
2024-04-30 14:53:31 +0900

    [FEAT] 관광지 타입 상수 추가

---
+++ src/main/java/com/est/neonaduri/spots/config/ContentCode.java
@@ -0,0 +1,19 @@
+package com.est.neonaduri.spots.config;
+
+public class ContentCode {
+    public static final int 관광지 = 12;
+
+    //확장성 고려 지역 별 컨텐츠 기반 정리
+    public static final int 문화시설 = 14;
+    public static final int 행사_공연_축제 = 15;
+    public static final int 여행코스 = 25;
+    public static final int 레포츠 = 28;
+    public static final int 숙박 = 32;
+    public static final int 쇼핑 = 38;
+    public static final int 음식점 = 39;
+
+    public static String getContentName(Long contentCode) {
+        if (contentCode == ContentCode.관광지) return "관광지";
+        else return "기타";
+    }
+}
