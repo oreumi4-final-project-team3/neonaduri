26e05ffe455d3514a95e9594b08a3e9557868b1b
emitlighthy <emitlighthy@naver.com>
2024-05-01 23:59:45 +0900

    [FEAT] UserController 개인정보 조회 api 수정

--- src/main/java/com/est/neonaduri/users/controller/UserController.java
+++ src/main/java/com/est/neonaduri/users/controller/UserController.java
@@ -4,6 +4,7 @@ import com.est.neonaduri.users.dto.UserDTO;
 import com.est.neonaduri.users.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +13,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping("/api/mypage")
-    public UserDTO myPage(UserDTO userDTO) {
-        return userService.getUserData(userDTO.getUserId());
+    @GetMapping("api/users/{userId}")
+    public UserDTO getUserById(@PathVariable String userId) {
+        return userService.getUserData(userId);
     }
+
 }
