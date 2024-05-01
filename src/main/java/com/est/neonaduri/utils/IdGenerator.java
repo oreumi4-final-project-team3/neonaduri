b829b592d43f279ee82b9b45b421a5db531303a9
임성현 <shlim0287@gmail.com>
2024-05-01 16:15:51 +0900

    [FEAT] IDGENERATOR 생성

---
+++ src/main/java/com/est/neonaduri/utils/IdGenerator.java
@@ -0,0 +1,26 @@
+package com.est.neonaduri.utils;
+
+import java.util.UUID;
+
+public class IdGenerator {
+    public static String generateUserId(){
+        return "user_"+ UUID.randomUUID().toString();
+    }
+
+    public static String generateSpotId() {
+        return "spot_"+ UUID.randomUUID().toString();
+    }
+    // 동행, 리뷰에 따라 달라지게
+    public static String generatePostId(String category){
+        return "post_"+category+"_"+UUID.randomUUID().toString();
+    }
+    // 어느 게시글의 위시리스트(찜) 여부에 따라 다르게
+    public static String generateWishListId(String category){
+        return "wishlist_"+category+"_"+UUID.randomUUID().toString();
+    }
+    // 어느 게시글의 댓글인지 여부에 따라 다르게
+    public static String generateReplyId(String category){
+        return "reply_"+category+"_"+UUID.randomUUID().toString();
+    }
+
+}
