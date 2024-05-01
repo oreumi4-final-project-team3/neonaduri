a211423404446b8d629b3c9ba7426a072cf119cb
임성현 <shlim0287@gmail.com>
2024-05-01 14:57:29 +0900

    [FEAT] API 호출 시 필요한 URL 추가

--- src/main/java/com/est/neonaduri/spots/config/ApiConst.java
+++ src/main/java/com/est/neonaduri/spots/config/ApiConst.java
@@ -8,7 +8,11 @@ public class ApiConst {
      * @author lsh
      */
     public static final String TOUR_API_ENDPOINT = "https://apis.data.go.kr/B551011/KorService1";
-    public static final String DEFAULT_QUERY_PARAMS="&MobileOS=ETC&MobileApp=neonaduri&_type=json";
+    public static final String DEFAULT_QUERY_PARAMS1 ="&MobileOS=ETC&MobileApp=neonaduri&_type=json";
+    public static final String DEFAULT_QUERY_PARAMS2 ="?MobileOS=ETC&MobileApp=neonaduri&_type=json";
+
+    public static final String DEFAULT_INFO_PARAMS="&defaultYN=Y&firstImageYN=Y&areacodeYN=Y&addrinfoYN=Y&mapinfoYN=Y&overviewYN=Y";
+
     public static final String LOCATION_INFO_BASED = "/locationBasedList1";
     public static final String AREA_BASED="/areaBasedList1";
     public static final String COMMON_INFO_BASED="/detailCommon1";
