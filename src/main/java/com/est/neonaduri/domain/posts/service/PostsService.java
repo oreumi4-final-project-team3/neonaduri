874dd5549947ad6768447d4f5d7e28d83cb1d53e
YH <jsm1252@gmail.com>
2024-05-03 03:08:30 +0900

    [FEAT] 게시글 생성 API(미완성)

--- src/main/java/com/est/neonaduri/domain/posts/service/PostsService.java
+++ src/main/java/com/est/neonaduri/domain/posts/service/PostsService.java
@@ -3,6 +3,9 @@ package com.est.neonaduri.domain.posts.service;
 import java.util.List;
 
 import com.est.neonaduri.domain.posts.domain.Posts;
+import com.est.neonaduri.domain.posts.dto.PostWriteDTO;
+import com.est.neonaduri.domain.users.domain.Users;
+import com.est.neonaduri.domain.users.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,15 +16,20 @@ public class PostsService {
 
 
 	private final PostsRepository postsRepository;
+	private final UserRepository userRepository;
 	@Autowired
-	public PostsService(PostsRepository postsRepository) {
+	public PostsService(PostsRepository postsRepository, UserRepository userRepository) {
 		this.postsRepository = postsRepository;
+		this.userRepository = userRepository;
 	}
 
 	//기본적인 CRUD 관련 코드들 작성
 	//Create
-	public void createPost(Posts posts){
-		postsRepository.save(posts);
+	public Posts createPost(String userId, PostWriteDTO postWriteDTO) {
+		// 사용자 ID로 사용자 정보 조회
+		Users user = userRepository.findById(userId)
+				.orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다. ID: " + userId));
+		return postsRepository.save(postWriteDTO.toEntity(user)) ;
 	}
 	//Read - ALL
 	public List<Posts> getAllPosts(){
