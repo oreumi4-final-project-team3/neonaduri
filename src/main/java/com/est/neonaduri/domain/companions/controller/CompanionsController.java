a27dd6dd092d483e9bde7527b2e3ddf9dbd83d8e
YH <jsm1252@gmail.com>
2024-05-04 02:25:06 +0900

    [FEAT] 게시글 생성 API(CompanionsController, CompanionsService 수정)

--- src/main/java/com/est/neonaduri/domain/companions/controller/CompanionsController.java
+++ src/main/java/com/est/neonaduri/domain/companions/controller/CompanionsController.java
@@ -1,11 +1,37 @@
 package com.est.neonaduri.domain.companions.controller;
 
+import com.est.neonaduri.domain.companions.domain.Companions;
 import com.est.neonaduri.domain.companions.service.CompanionsService;
-import lombok.RequiredArgsConstructor;
+import com.est.neonaduri.domain.posts.dto.CreatePostDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@RequiredArgsConstructor
 public class CompanionsController {
     private final CompanionsService companionsService;
+
+    @Autowired
+    public CompanionsController(CompanionsService companionsService) {
+        this.companionsService = companionsService;
+    }
+
+    /**
+     * 같이갈까? 게시글을 생성하는 API
+     *
+     * @return ResponseEntity<Companions> : 같이갈까? 게시글 생성
+     * @author jyh
+     */
+    @PostMapping("api/post/{userId}")
+    public ResponseEntity<Companions> createCompanions(@PathVariable(name = "userId") String userId, @RequestBody CreatePostDTO createPostDTO) {
+        log.info("입력된 사용자 : {}",userId);
+        Companions createdCompanions = companionsService.createCompanions(userId, createPostDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCompanions);
+    }
 }
--- src/main/java/com/est/neonaduri/domain/companions/service/CompanionsService.java
+++ src/main/java/com/est/neonaduri/domain/companions/service/CompanionsService.java
@@ -1,14 +1,48 @@
 package com.est.neonaduri.domain.companions.service;
 
+import com.est.neonaduri.domain.companions.domain.Companions;
 import com.est.neonaduri.domain.companions.repository.CompanionsRepository;
+import com.est.neonaduri.domain.posts.domain.Posts;
+import com.est.neonaduri.domain.posts.dto.CreatePostDTO;
+import com.est.neonaduri.domain.posts.repository.PostsRepository;
+import com.est.neonaduri.domain.users.domain.Users;
+import com.est.neonaduri.domain.users.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CompanionsService {
 
     private final CompanionsRepository companionsRepository;
+    private final UserRepository userRepository;
+    private final PostsRepository postsRepository;
 
-    public CompanionsService(CompanionsRepository companionsRepository) {
+    @Autowired
+    public CompanionsService(CompanionsRepository companionsRepository, UserRepository userRepository, PostsRepository postsRepository) {
         this.companionsRepository = companionsRepository;
+        this.userRepository = userRepository;
+        this.postsRepository = postsRepository;
+    }
+
+    // 동행 CRUD
+    // Create
+    @Transactional
+    public Companions createCompanions(String userId, CreatePostDTO createPostDTO) {
+        Users user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다. ID: " + userId));
+        // 게시글 생성
+        Posts savedPost = postsRepository.save(createPostDTO.toPostWriteDTO(user));
+        Companions savedCompanions = companionsRepository.save(createPostDTO.toCompanionsWriteDTO(savedPost));
+
+        return savedCompanions;
+
+
+//        Posts posts = postsRepository.findById(postId)
+//                .orElseThrow(() -> new IllegalArgumentException("게시물을 찾을 수 없습니다. ID: " + postId));
+//        Posts savedPost = postsRepository.save(postWriteDTO.toEntity(user));
+//        Companions companions = companionsWriteDTO.toEntity(posts);
+//        companions.setPosts(savedPost);
+//        return companionsRepository.save(companions);
     }
 }
