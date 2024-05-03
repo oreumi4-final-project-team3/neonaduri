da6eb7262990b67a5116ed67277b975d5d88e0b6
euichan0927 <yuichan99@naver.com>
2024-05-03 16:32:17 +0900

    [ADD] import 추가

--- src/main/java/com/est/neonaduri/domain/posts/controller/PostsController.java
+++ src/main/java/com/est/neonaduri/domain/posts/controller/PostsController.java
@@ -9,10 +9,14 @@ import com.est.neonaduri.domain.posts.dto.PostsListDTO;
 import com.est.neonaduri.domain.posts.dto.ReviewDTO;
 import com.est.neonaduri.domain.posts.service.PostsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
