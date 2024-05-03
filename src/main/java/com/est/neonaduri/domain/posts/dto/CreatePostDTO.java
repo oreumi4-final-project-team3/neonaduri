12244c8e57ccf4a2d174ad1ed9ad0ed23261a85d
YH <jsm1252@gmail.com>
2024-05-03 23:13:21 +0900

    [FEAT] 게시글 생성 API(CreatePostDTO 추가)

---
+++ src/main/java/com/est/neonaduri/domain/posts/dto/CreatePostDTO.java
@@ -0,0 +1,46 @@
+package com.est.neonaduri.domain.posts.dto;
+
+import com.est.neonaduri.domain.companions.domain.Companions;
+import com.est.neonaduri.domain.posts.domain.Posts;
+import com.est.neonaduri.domain.users.domain.Users;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreatePostDTO {
+    private String postCategory;
+    private String postTitle;
+    private String postContent;
+    private String spotName;
+    private Integer areaCode;
+    private String address;
+    private Integer comRecruit;
+    private LocalDateTime comStart;
+    private LocalDateTime comEnd;
+
+    public Posts toPostWriteDTO(Users users) {
+        return Posts.builder()
+                .postCategory(postCategory)
+                .postTitle(postTitle)
+                .postContent(postContent)
+                .areaCode(areaCode)
+                .spotName(spotName)
+                .address(address)
+                .users(users)
+                .build();
+    }
+
+    public Companions toCompanionsWriteDTO(Posts posts) {
+        return Companions.builder()
+                .comRecruit(comRecruit)
+                .comStart(comStart)
+                .comEnd(comEnd)
+                .posts(posts)
+                .build();
+    }
+}
