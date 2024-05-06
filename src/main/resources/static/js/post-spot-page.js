e09f1a7d8467a88864703735ce5a90ac497896fb
임성현 <shlim0287@gmail.com>
2024-05-06 18:24:12 +0900

    [FEAT] 카카오 api 에 관련된 js 기능 추가

--- src/main/resources/static/js/post-spot-page.js
+++ src/main/resources/static/js/post-spot-page.js
@@ -30,3 +30,65 @@ function addLineBreaks() {
     // 결과를 p 요소에 적용
     pElement.innerHTML = newText;
 }
+
+// areaCode에 해당하는 지역명과 관광지 사진 대체 로직
+
+const areaDetails = {
+    1: { name: "서울", image: "/images/seoul.png" },
+    31: { name: "경기", image: "/images/kyungki.jpg" },
+    2: { name: "인천", image: "/images/incheon.jpg" },
+    32: { name: "강원", image: "/images/kangwon.jpg" },
+    33: { name: "충북", image: "/images/chungbuk.jpg" },
+    34: { name: "충남", image: "/images/chungnam.jpg" },
+    35: { name: "경북", image: "/images/kyungbuk.jpg" },
+    36: { name: "경남", image: "/images/kyungnam.jpg" },
+    37: { name: "전북", image: "/images/jeonbuk.jpg" },
+    38: { name: "전남", image: "/images/jeonnam.jpg" },
+    39: { name: "제주", image: "/images/jeju.jpg" }
+};
+
+function updateAreaInfo() {
+    const area = areaDetails[areaCode]; // areaCode를 사용하여 지역 정보 찾기
+    if (area) {
+        document.getElementById('area-image').src = area.image;
+        document.getElementById('area-name').textContent = area.name;
+    } else {
+        console.log('지역 코드가 유효하지 않습니다.');
+    }
+}
+
+updateAreaInfo(); // 페이지 로드 시 함수 실행
+
+// 스크롤 버튼 관련
+
+// let scrollTop = document.getElementById("scrollTop");
+// let scrollDown = document.getElementById("scrollDown");
+//
+// window.addEventListener('scroll', scrollFunction);
+//
+// scrollTop.addEventListener('click', handleScrollTop);
+// scrollDown.addEventListener('click',handleScrollDown);
+//
+// function scrollFunction() {
+//     if (document.documentElement.scrollTop > 100) {
+//         scrollTop.classList.add("show");
+//         scrollDown.classList.add("show");
+//     } else {
+//         scrollTop.classList.remove("show");
+//         scrollDown.classList.remove("show");
+//     }
+// }
+// function handleScrollTop() {
+//     window.scrollTo({
+//         top: 0,
+//         behavior: "smooth"
+//     });
+// }
+//
+//
+// function handleScrollDown() {
+//     window.scrollTo({
+//         top: document.documentElement.scrollHeight,
+//         behavior: "smooth"
+//     });
+// }
