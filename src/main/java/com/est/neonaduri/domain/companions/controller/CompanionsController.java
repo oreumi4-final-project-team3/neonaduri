6f7c9bf61fddd93bfcf6b1672a095281361018de
YH <jsm1252@gmail.com>
2024-05-07 12:02:27 +0900

    [FEAT] 게시글 수정 API

--- src/main/java/com/est/neonaduri/domain/companions/controller/CompanionsController.java
+++ src/main/java/com/est/neonaduri/domain/companions/controller/CompanionsController.java
@@ -3,14 +3,12 @@ package com.est.neonaduri.domain.companions.controller;
 import com.est.neonaduri.domain.companions.domain.Companions;
 import com.est.neonaduri.domain.companions.service.CompanionsService;
 import com.est.neonaduri.domain.posts.dto.CreatePostDTO;
+import com.est.neonaduri.domain.posts.dto.UpdatePostDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -28,10 +26,24 @@ public class CompanionsController {
      * @return ResponseEntity<Companions> : 같이갈까? 게시글 생성
      * @author jyh
      */
-    @PostMapping("api/post/{userId}")
+    @PostMapping("api/posts/companions/{userId}")
     public ResponseEntity<Companions> createCompanions(@PathVariable(name = "userId") String userId, @RequestBody CreatePostDTO createPostDTO) {
         log.info("입력된 사용자 : {}",userId);
         Companions createdCompanions = companionsService.createCompanions(userId, createPostDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdCompanions);
+    }
+
+    /**
+     * 같이갈까? 게시글을 수정하는 API
+     *
+     * @return ResponseEntity<Companions> : 같이갈까? 게시글 수정
+     * @author jyh
+     */
+    @PutMapping("api/posts/companions/{userId}/{postId}")
+    public ResponseEntity<Companions> updateCompanions(@PathVariable(name = "userId") String userId, @PathVariable(name = "postId") String postId,
+                                                       @RequestBody UpdatePostDTO updatePostDTO) {
+        Companions updatedCompanions = companionsService.updateCompanions(userId, postId, updatePostDTO);
+        log.info("수정 게시물 : {}", postId);
+        return ResponseEntity.ok().body(updatedCompanions);
     }
 }
--- src/main/java/com/est/neonaduri/domain/companions/service/CompanionsService.java
+++ src/main/java/com/est/neonaduri/domain/companions/service/CompanionsService.java
@@ -4,13 +4,19 @@ import com.est.neonaduri.domain.companions.domain.Companions;
 import com.est.neonaduri.domain.companions.repository.CompanionsRepository;
 import com.est.neonaduri.domain.posts.domain.Posts;
 import com.est.neonaduri.domain.posts.dto.CreatePostDTO;
+import com.est.neonaduri.domain.posts.dto.UpdatePostDTO;
 import com.est.neonaduri.domain.posts.repository.PostsRepository;
 import com.est.neonaduri.domain.users.domain.Users;
 import com.est.neonaduri.domain.users.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Slf4j
 @Service
 public class CompanionsService {
 
@@ -31,18 +37,38 @@ public class CompanionsService {
     public Companions createCompanions(String userId, CreatePostDTO createPostDTO) {
         Users user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다. ID: " + userId));
-        // 게시글 생성
+
         Posts savedPost = postsRepository.save(createPostDTO.toPostWriteDTO(user));
         Companions savedCompanions = companionsRepository.save(createPostDTO.toCompanionsWriteDTO(savedPost));
 
         return savedCompanions;
+    }
 
+    // Update
+    @Transactional
+    public Companions updateCompanions(String userId, String postId, UpdatePostDTO updatePostDTO) {
+        Posts post = postsRepository.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException("게시글을 찾을 수 없습니다. ID: " + postId));
 
-//        Posts posts = postsRepository.findById(postId)
-//                .orElseThrow(() -> new IllegalArgumentException("게시물을 찾을 수 없습니다. ID: " + postId));
-//        Posts savedPost = postsRepository.save(postWriteDTO.toEntity(user));
-//        Companions companions = companionsWriteDTO.toEntity(posts);
-//        companions.setPosts(savedPost);
-//        return companionsRepository.save(companions);
+        if (!userId.equals(post.getUsers().getUserId())) {
+            throw new IllegalArgumentException("게시글 작성자와 로그인한 사용자가 일치하지 않습니다!");
+        }
+
+        post.setPostCategory(updatePostDTO.getPostCategory());
+        post.setPostTitle(updatePostDTO.getPostTitle());
+        post.setPostContent(updatePostDTO.getPostContent());
+        post.setSpotName(updatePostDTO.getSpotName());
+        post.setAreaCode(updatePostDTO.getAreaCode());
+        post.setAddress(updatePostDTO.getAddress());
+        post.setModified(LocalDateTime.now());
+        postsRepository.save(post);
+
+        Companions companions = companionsRepository.findByPosts_PostId(postId)
+                .orElseThrow(() -> new IllegalArgumentException("연결된 동행 정보를 찾을 수 없습니다. 게시글 ID: " + postId));
+
+        companions.setComRecruit(updatePostDTO.getComRecruit());
+        companions.setComStart(updatePostDTO.getComStart());
+        companions.setComEnd(updatePostDTO.getComEnd());
+        return companionsRepository.save(companions);
     }
 }
