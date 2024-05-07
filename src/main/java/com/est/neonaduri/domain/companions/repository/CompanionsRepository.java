d1e2b8aff63b872c64f768c59bcbface5441b605
YH <jsm1252@gmail.com>
2024-05-07 13:04:37 +0900

    [FIX] CompanionsRepository 미사용 import 제거

--- src/main/java/com/est/neonaduri/domain/companions/repository/CompanionsRepository.java
+++ src/main/java/com/est/neonaduri/domain/companions/repository/CompanionsRepository.java
@@ -1,7 +1,6 @@
 package com.est.neonaduri.domain.companions.repository;
 
 import com.est.neonaduri.domain.companions.domain.Companions;
-import com.est.neonaduri.domain.posts.domain.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
