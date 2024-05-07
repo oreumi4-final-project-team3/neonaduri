4445f6ae0c5dfd00410f627e47d36aa69a66bb87
YH <jsm1252@gmail.com>
2024-05-07 10:03:15 +0900

    [FEAT] 게시글 수정 API(CompanionsRepository 수정)

--- src/main/java/com/est/neonaduri/domain/companions/repository/CompanionsRepository.java
+++ src/main/java/com/est/neonaduri/domain/companions/repository/CompanionsRepository.java
@@ -1,9 +1,13 @@
 package com.est.neonaduri.domain.companions.repository;
 
 import com.est.neonaduri.domain.companions.domain.Companions;
+import com.est.neonaduri.domain.posts.domain.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CompanionsRepository extends JpaRepository<Companions, String> {
+    Optional<Companions> findByPosts_PostId(String postId);
 }
