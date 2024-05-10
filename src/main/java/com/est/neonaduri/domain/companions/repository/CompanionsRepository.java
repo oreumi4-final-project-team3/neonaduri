3f38c76252ac1e5c6c418dbbc14109f81f3bb714
YH <jsm1252@gmail.com>
2024-05-10 17:56:20 +0900

    [FIX] CompanionsRepository 수정

--- src/main/java/com/est/neonaduri/domain/companions/repository/CompanionsRepository.java
+++ src/main/java/com/est/neonaduri/domain/companions/repository/CompanionsRepository.java
@@ -1,10 +1,14 @@
 package com.est.neonaduri.domain.companions.repository;
 
 import com.est.neonaduri.domain.companions.domain.Companions;
+import com.est.neonaduri.domain.posts.domain.Posts;
 import com.est.neonaduri.domain.spots.domain.Spots;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,6 +18,11 @@ import java.util.Optional;
 public interface CompanionsRepository extends JpaRepository<Companions, String> {
     Optional<Companions> findByPosts_PostId(String postId);
 
+    Page<Companions> findCompanionsByPosts_PostCategory(String postCategory, Pageable pageable);
+
     @Query("SELECT c FROM Companions c JOIN c.posts p WHERE p.postCategory = 'companions' ORDER BY p.postView DESC limit 3")
     List<Companions> findHotCompanions();
+
+    @Query("SELECT c FROM Companions c JOIN c.posts p WHERE p.areaCode = :areaCode AND p.postCategory = 'companions'")
+    Page<Companions> findCompanionsByAreaCodeAndCategory(@Param("areaCode") int areaCode, Pageable pageable);
 }
