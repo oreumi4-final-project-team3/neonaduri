bae2426510c5820827e7e409847b4726c2b37445
임성현 <shlim0287@gmail.com>
2024-05-10 20:20:51 +0900

    [FEAT] 댓글 작성 컨트롤러 코드 추가

--- src/main/java/com/est/neonaduri/domain/replies/controller/RepliesController.java
+++ src/main/java/com/est/neonaduri/domain/replies/controller/RepliesController.java
@@ -1,11 +1,31 @@
 package com.est.neonaduri.domain.replies.controller;
 
+import com.est.neonaduri.domain.replies.domain.Replies;
+import com.est.neonaduri.domain.replies.dto.ReplyRequestDto;
 import com.est.neonaduri.domain.replies.service.RepliesService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class RepliesController {
     private final RepliesService repliesService;
+
+    /**
+     * 댓글 저장 기능
+     * @param postId
+     * @param userId
+     * @param replyRequestDto
+     * @return
+     * @author lsh
+     */
+    @PostMapping("api/posts/{postId}/{userId}")
+    public ResponseEntity<Replies> saveReply(@PathVariable(name = "postId")String postId,
+                                             @PathVariable(name = "userId")String userId,
+                                             @RequestParam(value = "content") ReplyRequestDto replyRequestDto) {
+        Replies replies = repliesService.saveReply(postId, userId, replyRequestDto);
+        return ResponseEntity.ok(replies);
+    }
+    
 }
