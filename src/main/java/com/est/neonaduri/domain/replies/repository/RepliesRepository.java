c1803bbc91941204b3e2c0e558ec85ea5175a4a2
임성현 <shlim0287@gmail.com>
2024-05-10 21:24:06 +0900

    [FEAT] 게시물에 작성 된 게시물 리스트 조회 레포지 메서드 추가

--- src/main/java/com/est/neonaduri/domain/replies/repository/RepliesRepository.java
+++ src/main/java/com/est/neonaduri/domain/replies/repository/RepliesRepository.java
@@ -4,6 +4,9 @@ import com.est.neonaduri.domain.replies.domain.Replies;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RepliesRepository extends JpaRepository<Replies, String> {
+    List<Replies> findByPosts_PostId(String postId);
 }
