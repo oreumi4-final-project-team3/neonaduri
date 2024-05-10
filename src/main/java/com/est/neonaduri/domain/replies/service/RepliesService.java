49ceda5a509b006a7a58eff2ab158d1f5a981c59
임성현 <shlim0287@gmail.com>
2024-05-10 21:00:47 +0900

    [FEAT] 댓글 수정 비지니스 로직 구현

--- src/main/java/com/est/neonaduri/domain/replies/service/RepliesService.java
+++ src/main/java/com/est/neonaduri/domain/replies/service/RepliesService.java
@@ -26,4 +26,29 @@ public class RepliesService {
         Replies reply = dto.toEntity(findPost, findUser);
         return repliesRepository.save(reply);
     }
+
+    @Transactional
+    public Replies updateReply(String postId,String replyId,String userId,ReplyRequestDto dto){
+        Posts findPost = postsRepository.findById(postId).orElseThrow(() -> new EntityNotFoundException("해당하는 게시물이 없습니다"));
+        Users findUser = userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("해당하는 유저 정보가 없습니다."));
+        Replies findReply = repliesRepository.findById(replyId).orElseThrow(() -> new EntityNotFoundException("해당하는 댓글이 없습니다"));
+
+        validateReply(userId,findReply);
+
+        Replies modifiedReply = dto.toEntity(findPost, findUser);
+        findReply.updateReply(modifiedReply.getContent());
+
+        return findReply;
+    }
+
+    private static void validateReply(String userId, Replies findReply)  {
+        if (!findReply.getUsers().getUserId().equals(userId)) {
+            throw new IllegalArgumentException("댓글을 작성한 유저가 아닙니다.");
+        }
+
+        // 댓글 존재 여부
+        if (findReply.getReplyId() == null) {
+            throw new IllegalArgumentException("해당하는 댓글이 없습니다.");
+        }
+    }
 }
