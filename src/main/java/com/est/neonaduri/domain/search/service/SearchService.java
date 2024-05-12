54108957cec79d9fc8c46f215b33e1df3c69dc05
임성현 <shlim0287@gmail.com>
2024-05-12 17:48:33 +0900

    [FEAT] 게시물 검색 비지니스 로직 추가

---
+++ src/main/java/com/est/neonaduri/domain/search/service/SearchService.java
@@ -0,0 +1,23 @@
+package com.est.neonaduri.domain.search.service;
+
+import com.est.neonaduri.domain.posts.dto.PostsListDTO;
+import com.est.neonaduri.domain.posts.repository.PostsRepository;
+import com.est.neonaduri.domain.search.dto.PostSearchCondition;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class SearchService {
+    private final PostsRepository postsRepository;
+
+    public List<PostsListDTO> searchPosts(PostSearchCondition postSearchCondition){
+        return postsRepository.search(postSearchCondition)
+                .stream()
+                .map(PostsListDTO::new)
+                .collect(Collectors.toList());
+    }
+}
