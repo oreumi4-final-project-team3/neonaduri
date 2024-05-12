72aacea350f0e5f22958cf3df7f872413856640a
임성현 <shlim0287@gmail.com>
2024-05-12 22:12:41 +0900

    [FEAT] redis활용한 실시간 검색어 순위 비지니스 로직 구현

--- src/main/java/com/est/neonaduri/domain/posts/service/SearchService.java
+++ src/main/java/com/est/neonaduri/domain/posts/service/SearchService.java
@@ -3,21 +3,34 @@ package com.est.neonaduri.domain.posts.service;
 import com.est.neonaduri.domain.posts.dto.PostsListDTO;
 import com.est.neonaduri.domain.posts.repository.PostsRepository;
 import com.est.neonaduri.domain.posts.dto.PostSearchCondition;
+import com.est.neonaduri.global.infra.redis.dto.SearchRankResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class SearchService {
     private final PostsRepository postsRepository;
+    private final RedisTemplate<String,String> redisTemplate;
 
     public List<PostsListDTO> searchPosts(PostSearchCondition postSearchCondition){
+        redisTemplate.opsForZSet().incrementScore("ranking",postSearchCondition.getSearchText(),1);
         return postsRepository.search(postSearchCondition)
                 .stream()
                 .map(PostsListDTO::new)
                 .collect(Collectors.toList());
+    }
+
+    public List<SearchRankResponseDto> getSearchRankList(){
+        String key="ranking";
+        ZSetOperations<String, String> zSetOperations = redisTemplate.opsForZSet();
+        Set<ZSetOperations.TypedTuple<String>> typedTuples = zSetOperations.reverseRangeWithScores(key, 0, 9);
+        return typedTuples.stream().map(SearchRankResponseDto::toResponseRankingDto).collect(Collectors.toList());
     }
 }
