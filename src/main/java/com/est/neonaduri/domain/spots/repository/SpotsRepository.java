93561142ddc83d4463cd442d1163f4cf9c8a076f
임성현 <shlim0287@gmail.com>
2024-05-13 16:23:40 +0900

    [REFACTOR] 카테고리명 수정.

--- src/main/java/com/est/neonaduri/domain/spots/repository/SpotsRepository.java
+++ src/main/java/com/est/neonaduri/domain/spots/repository/SpotsRepository.java
@@ -16,15 +16,15 @@ import org.springframework.stereotype.Repository;
 public interface SpotsRepository extends JpaRepository<Spots,String> {
 	Spots findByPosts_PostId(String postId);
 	// @Query를 사용한 예시
-	@Query("SELECT s FROM Spots s JOIN s.posts p WHERE p.areaCode = :areaCode AND p.postCategory = 'spots'")
+	@Query("SELECT s FROM Spots s JOIN s.posts p WHERE p.areaCode = :areaCode AND p.postCategory = 'spot'")
 	Page<Spots> findSpotsByAreaCodeAndCategory(@Param("areaCode") int areaCode, Pageable pageable);
 
-	@Query("SELECT s FROM Spots s JOIN s.posts p WHERE p.postCategory = 'spots' ORDER BY p.postView DESC limit 4")
+	@Query("SELECT s FROM Spots s JOIN s.posts p WHERE p.postCategory = 'spot' ORDER BY p.postView DESC limit 4")
 	List<Spots> findHotSpots();
 
-	@Query("SELECT s FROM Spots s JOIN s.posts p WHERE p.postCategory = 'spots' ORDER BY p.postView DESC limit 10")
+	@Query("SELECT s FROM Spots s JOIN s.posts p WHERE p.postCategory = 'spot' ORDER BY p.postView DESC limit 10")
 	List<Spots> findToptenSpots();
 
-	@Query("SELECT s FROM Spots s JOIN s.posts p WHERE p.spotName= :spotName AND p.postCategory = 'spots'")
+	@Query("SELECT s FROM Spots s JOIN s.posts p WHERE p.spotName= :spotName AND p.postCategory = 'spot'")
 	Spots findBySpotName(@Param("spotName") String spotName);
 }
