6a874e1aedf14ba85845fdf12f6a41f984976a21
YH <jsm1252@gmail.com>
2024-05-14 08:45:55 +0900

    [FIX] 같이갈까? 게시글 생성 페이지 뷰 2차 수정

--- src/main/resources/static/js/uploadCompanion.js
+++ src/main/resources/static/js/uploadCompanion.js
@@ -6,6 +6,6 @@ fileInput.addEventListener('change', function() {
     if (fileInput.files.length > 0) {
         uploadName.value = fileInput.files[0].name;
     } else {
-        uploadName.value = "첨부파일";
+        uploadName.value = "미 첨부 시, 기본 사진으로 게시됩니다.";
     }
 });
