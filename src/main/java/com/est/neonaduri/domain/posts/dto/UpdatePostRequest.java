36eda130c3be25374ceeed2063cab7ba6cb4007a
임성현 <shlim0287@gmail.com>
2024-05-15 18:45:39 +0900

    [FEAT] 기본생성자 어노테이션 추가

--- src/main/java/com/est/neonaduri/domain/posts/dto/UpdatePostRequest.java
+++ src/main/java/com/est/neonaduri/domain/posts/dto/UpdatePostRequest.java
@@ -2,7 +2,9 @@ package com.est.neonaduri.domain.posts.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class UpdatePostRequest {
